9a464c88ab0a4c6c7332596180b9c0c8ccc077c5
Jakub Adamík <jakub.adamik72@gmail.com>
2023-01-25 12:27:34 +0100

    feat: initial commit

---
+++ CollectionExtensions/IEnumerableExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Adamijak.Collections.Extensions;
+
+public static class IEnumerableExtensions
+{
+    public static void ForEach<T>(this IEnumerable<T> values, Action<T> func, CancellationToken cancelToken = default)
+    {
+        foreach (var value in values)
+        {
+            if (cancelToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            func(value);
+        }
+    }
+
+    public static async Task ForEachAsync<T>(this IEnumerable<T> values, Func<T, Task> func, CancellationToken cancelToken = default)
+    {
+        foreach (var value in values)
+        {
+            if (cancelToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            await func(value);
+        }
+    }
+
+    public static Task ForEachAsync<T>(this IEnumerable<T> values, Action<T> func, int maxWorkerCount = 5, CancellationToken cancelToken = default)
+        => ForEachAsync<T>(values, i => { func(i); return Task.CompletedTask; }, maxWorkerCount, cancelToken);
+
+    public static async Task ForEachAsync<T>(this IEnumerable<T> values, Func<T, Task> func, int maxWorkerCount = 5, CancellationToken cancelToken = default)
+    {
+        if(maxWorkerCount < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(maxWorkerCount), maxWorkerCount, "Can not be less than 1.");
+        }
+        var queue = new ConcurrentQueue<T>(values);
+
+        var tasks = new List<Task>();
+        foreach (var i in Enumerable.Range(0, Math.Min(queue.Count, maxWorkerCount)))
+        {
+            tasks.Add(ForEachTaskAsync(queue, func, cancelToken));
+        }
+        await Task.WhenAll(tasks);
+    }
+
+    private static async Task ForEachTaskAsync<T>(ConcurrentQueue<T> queue, Func<T, Task> func, CancellationToken cancelToken = default)
+    {
+        while (!queue.IsEmpty)
+        {
+            if (cancelToken.IsCancellationRequested)
+            {
+                return;
+            }
+            if (queue.TryDequeue(out var value))
+            {
+                await func(value);
+            }
+        }
+    }
+}
+
---
+++ CollectionExtensionsTests/ForEachTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using Adamijak.Collections.Extensions;
+
+namespace CollectionExtensionsTests;
+
+[TestClass]
+public class ForEachTests
+{
+    private const int Delay = 1000;
+    private const int ValueCount = 1000;
+    private readonly IEnumerable<int> Values = Enumerable.Range(0, ValueCount);
+
+    [TestMethod]
+    public async Task ProcessesAll()
+    {
+        var rnd = new Random();
+        var concurrentList = new ConcurrentBag<int>();
+        await Values.ForEachAsync(async i =>
+        {
+            await Task.Delay(rnd.Next(0, 200));
+            concurrentList.Add(i);
+        }, 100);
+        Assert.AreEqual(ValueCount, concurrentList.Count);
+    }
+
+    [TestMethod]
+    public async Task RunsConcurrently()
+    {
+        var watch = Stopwatch.StartNew();
+        await Values.ForEachAsync(async i =>
+        {
+            await Task.Delay(Delay);
+        }, 200);
+        watch.Stop();
+        Assert.IsTrue(Delay * 4.5 < watch.Elapsed.TotalMilliseconds);
+        Assert.IsTrue(watch.Elapsed.TotalMilliseconds < Delay * 5.5);
+    }
+
+    [TestMethod]
+    [DataTestMethod]
+    [DataRow(5)]
+    [DataRow(1)]
+    [DataRow(0)]
+    [DataRow(-5)]
+    public async Task MaxWorkerCount(int maxWorkerCount)
+    {
+        if (maxWorkerCount < 1)
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentOutOfRangeException>(() => Values.ForEachAsync(i => Task.CompletedTask, maxWorkerCount));
+        }
+        else
+        {
+            await Values.ForEachAsync(i => Task.CompletedTask, maxWorkerCount);
+        }
+    }
+}
